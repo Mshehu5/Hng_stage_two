e3b1e61a4edc77cf97f235f0fa7ec5efddbd5ff4
Mshehu5 <104724920+Mshehu5@users.noreply.github.com>
2022-11-05 09:25:30 +0100

    update

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-app.use("/", Router);
+const calculationscontroller = require("./controller/calculationscontroller");
+
+app.get("/", (req, res) => {
+  res.send("Hello Hng stage 3 task is running!");
+});
+
+app.post("/calc", calculationscontroller);
 
 app.listen(5000, () => {
   console.log("server is listening on port 5000..");
